9aefcefed085beaad38aac51f89ad72af6a52812
KrzysztofJurowicz <95973469+KrzysztofJurowicz@users.noreply.github.com>
2021-12-19 00:33:47 +0100

    Add files via upload

---
+++ Silnia.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Silnia1
+{
+
+    class Silnia
+    {
+
+        static BigInteger silnia(BigInteger n)
+        {
+            BigInteger result = 1;
+            for (BigInteger i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            String line;
+            BigInteger n;
+
+            Console.WriteLine("Podaj dla jakiej liczby obliczyc silnie.\n");
+            line = Console.ReadLine();
+            n = BigInteger.Parse(line);
+
+            Console.WriteLine(n + "! = " + silnia(n));
+            Console.WriteLine("Podaj dla jakiej liczby obliczyc silnie.\n");
+            line = Console.ReadLine();
+            n = BigInteger.Parse(line);
+
+            Console.WriteLine(n + "! = " + silnia(n));
+            Console.WriteLine("Podaj dla jakiej liczby obliczyc silnie.\n");
+            line = Console.ReadLine();
+            n = BigInteger.Parse(line);
+
+            Console.WriteLine(n + "! = " + silnia(n));
+            Console.ReadLine();
+        }
+    }
+}
+
+
